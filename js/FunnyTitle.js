a9df4ccc1c9e390e733116a63bcb27f22c1ed1da
LiamWang666 <18002337539@163.com>
2019-12-16 22:24:33 +0800

    Site updated: 2019-12-16 22:24:29

--- js/FunnyTitle.js
+++ js/FunnyTitle.js
@@ -4,12 +4,12 @@
  document.addEventListener('visibilitychange', function () {
      if (document.hidden) {
          $('[rel="icon"]').attr('href', "/img/trhx2.png");
-         document.title = '别走啊哥！回来继续玩啊！';
+         document.title = '别走啊哥！';
          clearTimeout(titleTime);
      }
      else {
          $('[rel="icon"]').attr('href', "/img/trhx2.png");
-         document.title = '谢谢大哥再次光临！';
+         document.title = '好兄弟！';
          titleTime = setTimeout(function () {
              document.title = OriginTitle;
          }, 2000);
